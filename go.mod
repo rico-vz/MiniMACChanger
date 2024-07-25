7fbcf43342e78d88cda1c74102f875f610afc99d
Rico van Zelst <rvzelst7@gmail.com>
2024-07-25 20:13:39 +0200

    working prototype

--- go.mod
+++ go.mod
@@ -2,15 +2,29 @@ module rico-vz/MiniMACChanger
 
 go 1.22.4
 
-require gioui.org v0.7.0
-
 require (
-	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
-	gioui.org/shader v1.0.8 // indirect
-	github.com/go-text/typesetting v0.1.1 // indirect
-	golang.org/x/exp v0.0.0-20221012211006-4de253d81b95 // indirect
-	golang.org/x/exp/shiny v0.0.0-20220827204233-334a2380cb91 // indirect
-	golang.org/x/image v0.5.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	github.com/AllenDang/cimgui-go v0.0.0-20240424153022-294abe7370df // indirect
+	github.com/AllenDang/giu v0.8.1 // indirect
+	github.com/AllenDang/go-findfont v0.0.0-20200702051237-9f180485aeb8 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/charmbracelet/log v0.4.0 // indirect
+	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mazznoer/csscolorparser v0.1.3 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/napsy/go-css v0.0.0-20221107082635-4ed403047a64 // indirect
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sahilm/fuzzy v0.1.1 // indirect
+	golang.design/x/hotkey v0.4.1 // indirect
+	golang.design/x/mainthread v0.3.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/image v0.18.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	gopkg.in/eapache/queue.v1 v1.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,54 +1,358 @@
 package main
 
 import (
-	"image/color"
-	"log"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"net"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"syscall"
+	"time"
 
-	"gioui.org/app"
-	"gioui.org/op"
-	"gioui.org/text"
-	"gioui.org/widget/material"
+	"github.com/AllenDang/giu"
+	"github.com/charmbracelet/log"
+	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/registry"
 )
 
+type macType string
+
+const (
+	original macType = "original"
+	previous macType = "previous"
+)
+
+func checkAdmin() bool {
+	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+
+	return err == nil
+}
+
+func becomeAdmin() {
+	verb := "runas"
+	exe, _ := os.Executable()
+	cwd, _ := os.Getwd()
+	args := strings.Join(os.Args[1:], " ")
+
+	verbPtr, _ := syscall.UTF16PtrFromString(verb)
+	exePtr, _ := syscall.UTF16PtrFromString(exe)
+	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+	argPtr, _ := syscall.UTF16PtrFromString(args)
+
+	var showCmd int32 = 1 //SW_NORMAL
+
+	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func loop() {
+	giu.SingleWindow().Layout(
+		giu.Button("").OnClick(func() {
+			log.Info("Change MAC Address button clicked")
+			changeMacAddress()
+		}).ID("Change MAC Address"),
+
+		giu.Button("").OnClick(func() {
+			fmt.Println("Restore original MAC Address button clicked")
+			restoreMacAddress(original)
+			originalAddress, err := readValueFromFile("original_address")
+			if err != nil {
+				log.Fatal("Failed to read original MAC Address from file. ", "Error", err)
+			}
+			log.Info("Original MAC Address restored. ", "Address", prettifyMacAddress(originalAddress))
+		}).ID("Restore original MAC Address"),
+
+		giu.Button("").OnClick(func() {
+			fmt.Println("Restore previous MAC Address button clicked")
+			restoreMacAddress(previous)
+			previousAddress, err := readValueFromFile("previous_address")
+			if err != nil {
+				log.Fatal("Failed to read previous MAC Address from file. ", "Error", err)
+			}
+			log.Info("Previous MAC Address restored. ", "Address", prettifyMacAddress(previousAddress))
+		}).ID("Restore previous MAC Address"),
+	)
+}
+
 func main() {
-	go func() {
-		window := new(app.Window)
-		err := run(window)
+	if !checkAdmin() {
+		becomeAdmin()
+		time.Sleep(2 * time.Second)
+
+		os.Exit(0)
+
+	}
+
+	giu.NewMasterWindow("Mini MAC Changer", 640, 480, 0).Run(loop)
+
+}
+
+func getMacAddress() ([]string, error) {
+	ifas, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	var as []string
+	for _, ifa := range ifas {
+		a := ifa.HardwareAddr.String()
+		if a != "" {
+			// Remove ":" and convert to uppercase
+			a = strings.ToUpper(strings.ReplaceAll(a, ":", ""))
+			as = append(as, a)
+		}
+	}
+	return as, nil
+}
+
+func findMatchingNetworkAddress(originalAddress string) (string, error) {
+	const basePath = `SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}`
+
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, basePath, registry.READ)
+	if err != nil {
+		return "", fmt.Errorf("failed to open base key: %v", err)
+	}
+	defer k.Close()
+
+	for i := 0; ; i++ {
+		subKeyName := fmt.Sprintf("%04d", i)
+		subKey, err := registry.OpenKey(k, subKeyName, registry.READ)
+		if err != nil {
+			if err == registry.ErrNotExist {
+				break
+			}
+			return "", fmt.Errorf("failed to open subkey %s: %v", subKeyName, err)
+		}
+		defer subKey.Close()
+
+		networkAddress, _, err := subKey.GetStringValue("NetworkAddress")
+		if err == nil && networkAddress == originalAddress {
+			return fmt.Sprintf(`%s\%s`, basePath, subKeyName), nil
+		}
+	}
+
+	return "", fmt.Errorf("no matching NetworkAddress found")
+}
+
+func saveValueToFile(filename string, value string) error {
+	key := []byte("MMC-GoLang")
+	key = append(key, make([]byte, 32-len(key))...)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return err
+	}
+
+	encryptedValue := gcm.Seal(nonce, nonce, []byte(value), nil)
+	hexEncryptedValue := hex.EncodeToString(encryptedValue)
+
+	if filepath.Ext(filename) != ".bkp" {
+		filename += ".bkp"
+	}
+
+	err = os.WriteFile(filename, []byte(hexEncryptedValue), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func readValueFromFile(filename string) (string, error) {
+	if filepath.Ext(filename) != ".bkp" {
+		filename += ".bkp"
+	}
+
+	encryptedHex, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	encryptedValue, err := hex.DecodeString(string(encryptedHex))
+	if err != nil {
+		return "", err
+	}
+
+	key := []byte("MMC-GoLang")
+	key = append(key, make([]byte, 32-len(key))...)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(encryptedValue) < nonceSize {
+		return "", errors.New("the cipher text is too short")
+	}
+
+	nonce, ciphertext := encryptedValue[:nonceSize], encryptedValue[nonceSize:]
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
+
+func prettifyMacAddress(macAddress string) string {
+	var result string
+	for i, c := range macAddress {
+		if i > 0 && i%2 == 0 {
+			result += "-"
+		}
+		result += string(c)
+	}
+	return strings.ToUpper(result)
+}
+
+func changeMacAddress() {
+	as, err := getMacAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info("MAC Address found. ", "Address", prettifyMacAddress(as[0]))
+
+	// Find the matching network address in registry (Windows grabs the MAC Address from here, so if we change it here, it will change the MAC Address)
+	keyPath, err := findMatchingNetworkAddress(as[0])
+	if err != nil {
+		log.Fatal("Failed to find matching network address. ", "Error", err)
+	}
+	log.Info("Found registry key path. ", "Path", keyPath)
+
+	_, err = readValueFromFile("original_address")
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			err = saveValueToFile("original_address", as[0])
+			if err != nil {
+				log.Fatal("Failed to save original MAC Address to file. ", "Error", err)
+			}
+		} else {
+			log.Fatal("Failed to read original MAC Address from file. ", "Error", err)
+		}
+	} else {
+		activeAddress, err := getMacAddress()
 		if err != nil {
 			log.Fatal(err)
 		}
-		os.Exit(0)
-	}()
-	app.Main()
-}
-
-func run(window *app.Window) error {
-	theme := material.NewTheme()
-	var ops op.Ops
-	for {
-		switch e := window.Event().(type) {
-		case app.DestroyEvent:
-			return e.Err
-		case app.FrameEvent:
-			// This graphics context is used for managing the rendering state.
-			gtx := app.NewContext(&ops, e)
-
-			// Define an large label with an appropriate text:
-			title := material.H1(theme, "Hello, Gio")
-
-			// Change the color of the label.
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
-
-			// Change the position of the label.
-			title.Alignment = text.Middle
-
-			// Draw the label to the graphics context.
-			title.Layout(gtx)
-
-			// Pass the drawing operations to the GPU.
-			e.Frame(gtx.Ops)
+		err = saveValueToFile("previous_address", activeAddress[0])
+		if err != nil {
+			log.Fatal("Failed to save previous MAC Address to file. ", "Error", err)
 		}
 	}
+
+	randomMAC, err := generateRandomMAC()
+	if err != nil {
+		log.Fatal("Failed to generate random MAC Address. ", "Error", err)
+	}
+	log.Info("Random MAC Address generated. ", "Address", randomMAC)
+
+	if err := setRegistryValue(keyPath, "NetworkAddress", randomMAC); err != nil {
+		log.Fatal("Failed to set registry value. ", "Error", err)
+	}
+
+	refreshConnection()
+}
+
+func restoreMacAddress(macType macType) {
+	var filename string
+	switch macType {
+	case original:
+		log.Info("Attemtping to restore original MAC Address")
+		filename = "original_address"
+	case previous:
+		log.Info("Attemtping to restore previous MAC Address")
+		filename = "previous_address"
+	default:
+		log.Fatal("Invalid macType. ", "macType", macType)
+	}
+
+	macAddress, err := readValueFromFile(filename)
+	if err != nil {
+		log.Fatal("Failed to read MAC Address from file. ", "Error", err)
+	}
+	log.Info("MAC Address read from file. ", "Address", prettifyMacAddress(macAddress))
+
+	activeAddress, err := getMacAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+	keyPath, err := findMatchingNetworkAddress(activeAddress[0])
+	if err != nil {
+		log.Fatal("Failed to find matching network address. ", "Error", err)
+	}
+	log.Info("Found registry key path. ", "Path", keyPath)
+
+	if err := setRegistryValue(keyPath, "NetworkAddress", macAddress); err != nil {
+		log.Fatal("Failed to set registry value. ", "Error", err)
+	}
+
+	refreshConnection()
+}
+
+func refreshConnection() {
+	log.Info("————————————————————————————————————————————————————————————")
+	log.Info("Refreshing network connection")
+	log.Info("(You might lose internet for a few seconds)")
+
+	exec.Command("cmd", "/C", "WMIC PATH WIN32_NETWORKADAPTER WHERE PHYSICALADAPTER=TRUE CALL DISABLE >nul 2>&1").Run()
+	time.Sleep(time.Second)
+	exec.Command("cmd", "/C", "WMIC PATH WIN32_NETWORKADAPTER WHERE PHYSICALADAPTER=TRUE CALL ENABLE >nul 2>&1").Run()
+	time.Sleep(time.Second)
+
+	log.Info("Network connection refreshed")
+}
+
+func setRegistryValue(keyPath string, valueName string, value string) error {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, keyPath, registry.WRITE)
+	if err != nil {
+		return fmt.Errorf("failed to open key: %v", err)
+	}
+	defer k.Close()
+
+	if err := k.SetStringValue(valueName, value); err != nil {
+		return fmt.Errorf("failed to set value: %v", err)
+	}
+
+	return nil
+}
+
+func generateRandomMAC() (string, error) {
+	mac := make([]byte, 6)
+
+	_, err := rand.Read(mac)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate random bytes: %w", err)
+	}
+
+	// Ensure the first byte has the locally administered bit set
+	mac[0] = (mac[0] | 0x02) & 0xfe
+
+	// Format the MAC address to fit format used in the registry (no spacers, uppercase)
+	return strings.ToUpper(fmt.Sprintf("%02x%02x%02x%02x%02x%02x",
+		mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])), nil
 }
